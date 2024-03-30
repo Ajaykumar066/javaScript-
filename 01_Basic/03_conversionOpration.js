758d0bddb14524a04bf3ad04c912b1a167daaafe
Ajay Kumar <ajayk066294@gmail.com>
2024-03-31 00:19:35 +0530

    thisis today commit

--- 01_Basic/03_conversionOpration.js
+++ 01_Basic/03_conversionOpration.js
@@ -7,3 +7,34 @@ let scoreNumber=Number(score)
 console.log(typeof valueNumber);
 console.log(typeof scoreNumber);
 console.log(scoreNumber)
+
+// Operations******
+
+let values = 3;
+let negValue = -values;
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " AjayKumar"
+
+console.log(str1+str2)
+
+// console.log("1"+ 2);
+// console.log(1 +'2')
+// console.log("1"+2+2)
+// console.log(1+2+'2')
+// console.log(1+2*5%3)
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3
+// num1 = num2 = num3 =2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+let gameCounter1 =100
+++gameCounter1
+console.log(gameCounter1)
